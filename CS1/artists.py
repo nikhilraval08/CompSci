ec0f73f52de77306a5644b94c0add86e84dc0171
nikhilraval08 <nraval27@gcds.net>
2024-09-30 20:07:12 -0400

    Delete CS1 directory

--- CS1/artists.py
+++
@@ -1,8 +0,0 @@
-import random
-artists = ["Travis Scott", "Drake", "Eminem"]
-print (artists)
-print (artists [1])
-artists.append("Dr. Dre")
-print(random.choice (artists))
-artists.pop(0)
-print (artists)
--- CS1/hello world.py
+++
@@ -1,8 +0,0 @@
-print ("hello world")
-hello = input ("say hello? ")
-
-if hello == "yes":
-        print("hello world")
-
-
-print ("bye")
